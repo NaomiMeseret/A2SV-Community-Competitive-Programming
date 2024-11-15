65597e3f0f1a6f0800f48ad8a882b09284c8461f
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2024-11-15 18:15:38 +0300

    Time: 3 ms (42.00%) | Memory: 16.6 MB (97.51%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,5 +1,5 @@
-class Solution(object):
-    def relativeSortArray(self, arr1, arr2):
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         maximum=max(arr1)
         count=[0]*(maximum+1)
         for num in arr1:
@@ -8,8 +8,10 @@ class Solution(object):
         for num in arr2:
             if count[num]>0:
                 sorted_arr.extend([num]*count[num])
-                count[num]=0
         for num in range(maximum+1):
-            if count[num]>0:
+            if count[num]>0 and num not in arr2:
                 sorted_arr.extend([num]*count[num])
         return sorted_arr
+
+        
+        
