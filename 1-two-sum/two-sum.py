1ef028156057af5432776d78e97c01fac957d70b
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2024-11-17 22:18:57 +0300

    Time: 2737 ms (6.69%) | Memory: 17.2 MB (97.85%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,7 +1,5 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        left=0
-        right=0
         for left in range(len(nums)):
             right=left+1
             while right<len(nums):
