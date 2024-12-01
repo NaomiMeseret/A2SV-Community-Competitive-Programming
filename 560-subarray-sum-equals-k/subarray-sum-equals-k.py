3ac0177626fd47e7c3f351940b771f33e869af18
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2024-12-01 23:13:34 +0300

    Time: 30 ms (69.93%) | Memory: 19.9 MB (18.69%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefixSum=0
+        prefixSumCount={0:1}
+        count=0
+        for num in nums:
+            prefixSum+=num
+            if (prefixSum-k) in prefixSumCount:
+                count+=prefixSumCount[prefixSum-k]
+            prefixSumCount[prefixSum]=prefixSumCount.get(prefixSum,0)+1
+        return count
+        
