a8e2ca2db5be8538391fa5d4922555798fd2f4d1
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2024-12-28 14:54:15 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (7.61%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -0,0 +1,13 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack=[]
+        for char in s:
+            if stack:
+                if char.lower()==stack[-1].lower() and char!=stack[-1]:
+                    stack.pop()
+                else:
+                    stack.append(char)
+            else:
+                stack.append(char)
+        return "".join(stack)
+        
