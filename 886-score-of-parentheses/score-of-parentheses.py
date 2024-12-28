2992add2357749b306081885e3059667a28a033d
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2024-12-29 02:34:48 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (5.72%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack=[]
+        for char in s:
+            if char=="(":
+                stack.append(0)
+            else:
+                last=stack.pop()
+                if last==0:
+                    stack.append(1)
+                else:
+                    stack.append(2*last)
+                if len(stack)>1:
+                    top=stack.pop()
+                    stack[-1]+=top
+        return stack[0]
+
+
+        
