6dda355c603b9a058a69ebc30393762206bac215
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-03-25 18:57:38 +0300

    Time: 0 ms (100.00%) | Memory: 19 MB (52.83%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,35 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        first=-1
+        last=-1
+        def firstPostion(l,r):
+            nonlocal first
+            while l<=r:
+                mid=(l+r)//2
+                if nums[mid]<target:
+                    l=mid+1
+                else:
+                    if nums[mid]==target:
+                        first=mid
+                    r=mid-1
+            return first
+        def lastPostion(l,r):
+            nonlocal last
+            while l<=r:
+                mid=(l+r)//2
+                if nums[mid]>target:
+                    r=mid-1
+                else:
+                    if nums[mid]==target:
+                        last=mid
+                    l=mid+1
+            return last
+        ans=[firstPostion(0,len(nums)-1), lastPostion(0,len(nums)-1)]
+        return ans
+
+        
+        
+        
+
+     
+        
