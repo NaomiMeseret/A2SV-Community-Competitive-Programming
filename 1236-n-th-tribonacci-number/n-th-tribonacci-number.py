dfed1a496f1db268274b2852909f61285b63db9e
Naomi_Meseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-09-24 23:22:47 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (47.43%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        memo = {}
+        def dp(n):
+            if n in memo:
+                return memo[n]
+            if n == 0:
+                return 0
+            if n == 1 or n == 2:
+                return 1
+            memo[n] = dp(n-1)+dp(n-2)+dp(n-3)
+            return memo[n]
+        return dp(n)
