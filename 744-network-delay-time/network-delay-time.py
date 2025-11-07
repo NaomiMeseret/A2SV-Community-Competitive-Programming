cb193744aa542b95ed5a4920fa4e9af6cd8ff172
Naomi_Meseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-11-07 22:34:42 +0300

    Time: 398 ms (24.38%) | Memory: 19.7 MB (75.98%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,30 +1,21 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        adjacency = []
-        for i in range(0, n + 1):
-            adjacency.append([])
-        for edge in times:
-            u,v,w = edge[0], edge[1], edge[2]
-            adjacency[u].append((v, w))
-        dist = [float('inf')] * (n + 1)
-        dist[k] = 0
-        heap = []
-        heapq.heappush(heap, (0, k))
-        while len(heap) > 0:
-            current = heapq.heappop(heap)
-            d = current[0]
-            node = current[1]
-            if d > dist[node]:
+        graph = [[] for _ in range(n + 1)]
+        for u, v, w in times:
+            graph[u].append((w, v))
+        heap = [(0, k)]
+        visited = set()
+        time = 0
+        while heap:
+            t, u = heapq.heappop(heap)
+            if u in visited:
                 continue
-            neighbors = adjacency[node]
-            for item in neighbors:
-                v,w = item[0],item[1]
-                nd = d + w
-                if nd < dist[v]:
-                    dist[v] = nd
-                    heapq.heappush(heap, (nd, v))
-        ans = max(dist[1:])
-        if ans == float('inf'):
-            return -1
+            visited.add(u)
+            time = t
+            for w, v in graph[u]:
+                if v not in visited:
+                    heapq.heappush(heap, (t + w, v))
+        if len(visited) == n:
+            return time 
         else:
-            return ans
+            return -1
