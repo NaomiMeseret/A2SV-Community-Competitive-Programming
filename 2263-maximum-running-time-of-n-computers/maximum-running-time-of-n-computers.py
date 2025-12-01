c0a917f0537706626fe5e8c033e33ba6ae40c9d1
Naomi_Meseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-12-01 21:38:01 +0300

    Time: 1355 ms (53.33%) | Memory: 30.9 MB (91.43%) - LeetSync

---
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        l, r =min(batteries), sum(batteries)//n 
+        ans = 0
+        while l<=r:
+            mid=(l+r)>>1
+            reserve=0
+            for x in batteries: 
+                reserve+=min(x, mid)
+            if reserve>=mid*n:
+                ans=mid
+                l=mid+1
+            else:
+                r=mid-1
+        return ans
+        
