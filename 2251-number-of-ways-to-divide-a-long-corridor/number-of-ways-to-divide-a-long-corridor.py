867f0f438cc118629c1d5fe94139edf93f647d06
Naomi_Meseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-12-14 21:50:50 +0300

    Time: 170 ms (98.10%) | Memory: 18.1 MB (84.76%) - LeetSync

---
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfWays(self, corridor: str) -> int:
+        mod = 10**9 + 7
+        s0 = 0
+        s1 = 0 
+        s2 = 1
+        for c in corridor:
+            if c == 'S': 
+                s0, s1, s2 = s1, s2, s1
+            else: 
+                s2 = (s2 + s0) % mod
+        return s0
+        
